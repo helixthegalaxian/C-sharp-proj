8766da0d2ac4ab0e81401db492331dcfdd111291
DaJavaScriptcode <111297917+helixthegalaxian@users.noreply.github.com>
2022-12-04 13:22:56 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace danewgame
+{
+
+    class Program
+    {
+
+            static void Main(string[] args)
+            {
+                Console.WriteLine("hello there");
+                Input = Console.WriteLine("hello what's your name");
+                if (Input == "weirdo"){
+                    Console.WriteLine("oh no please don't kill me!");
+                }else
+                {
+                    Console.WriteLine("hi there!");
+                }
+
+            }
+
+    }
+
+
+
+}
